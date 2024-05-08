1216e9fde59c6c692ccd799c4efdb8f0921191bf
bshongwe <dru.da.beat.hustle@gmail.com>
2024-05-08 01:27:18 -0700

    Task 10 - Safe divide: ✨🔢 Implemented SafeDiv function to safely divide two numbers, returning the result or 0 if the divisor is 0 🚀

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,15 @@
+-- creates function SafeDiv to divide and return 1st num
+-- by 2nd num, returns 0 if 2nd num is equal to 0.
+DROP FUNCTION IF EXISTS SafeDiv;
+DELIMITER $$
+CREATE FUNCTION SafeDiv (a INT, b INT)
+RETURNS FLOAT DETERMINISTIC
+BEGIN
+    DECLARE result FLOAT DEFAULT 0;
+
+    IF b != 0 THEN
+        SET result = a / b;
+    END IF;
+    RETURN result;
+END $$
+DELIMITER ;
