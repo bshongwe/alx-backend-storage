dcba5f294436ee1097b32918708ef14c740afa5d
bshongwe <dru.da.beat.hustle@gmail.com>
2024-05-08 10:35:58 -0700

    Task 3 - Old school band: ✨ Query to list Glam rock bands ranked longevity, considering potential split years - changed 2020 to 2022

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -2,10 +2,10 @@
 -- Selects band_name and calculates their lifespan by subtracting year formed
 -- *** from the year of their split (if exists) or the current year.
 -- L6: Uses IFNULL to handle NULL values in 'split' column, defaulting
--- *** to '2020'.
+-- *** to '2022'.
 -- L7: Filters bands with 'Glam rock' as one of their styles.
 -- L8: Orders the result by lifespan in descending order.
-SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
+SELECT band_name, (IFNULL(split, '2022') - formed) AS lifespan
     FROM metal_bands
     WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
     ORDER BY lifespan DESC;
