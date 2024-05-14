c29e66928b275e34c3fa0925da91807ae6df52eb
bshongwe <dru.da.beat.hustle@gmail.com>
2024-05-13 18:34:56 -0700

    Task 12: fix

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -15,7 +15,7 @@ def print_nginx_request_logs(nginx_collection):
         print('\tmethod {}: {}'.format(method, req_count))
     status_checks_count = len(list(
         nginx_collection.find({'method': 'GET', 'path': '/status'})
-    ))
+        ))
     print('{} status check'.format(status_checks_count))
 
 
