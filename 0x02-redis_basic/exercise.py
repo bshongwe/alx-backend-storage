953f4aec8f546b77d30047199b5a6d4eb9d9fe0a
bshongwe <dru.da.beat.hustle@gmail.com>
2024-05-14 23:30:31 -0700

    Task 0:1✨ ARevisedCache class for Redis data storage with store method 🔥🗃️ - reading

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,22 +4,34 @@ Module: Redis NoSQL data storage.
 """
 import uuid
 import redis
-from typing import Union
+from typing import Union, Callable, Any
 
 
 def call_history(func):
-    def wrapper(*args, **kwargs):
-        # Placeholder for actual call history logic
-        print(f"Calling {func.__name__}")
-        return func(*args, **kwargs)
-    return wrapper
+    """Tracks call details of Cache class method."""
+    @wraps(method)
+    def invoker(self, *args, **kwargs) -> Any:
+        """Returns method output after storing its inputs and output."""
+        in_key = '{}:inputs'.format(method.__qualname__)
+        out_key = '{}:outputs'.format(method.__qualname__)
+        if isinstance(self._redis, redis.Redis):
+            self._redis.rpush(in_key, str(args))
+        output = method(self, *args, **kwargs)
+        if isinstance(self._redis, redis.Redis):
+            self._redis.rpush(out_key, output)
+        return output
+    return invoker
+
 
 def count_calls(func):
-    def wrapper(*args, **kwargs):
-        # Placeholder for actual count calls logic
-        print(f"Counting call to {func.__name__}")
-        return func(*args, **kwargs)
-    return wrapper
+    """Tracks number of calls made to Cache class method."""
+    @wraps(method)
+    def invoker(self, *args, **kwargs) -> Any:
+        """Invokes given method after incrementing its call counter."""
+        if isinstance(self._redis, redis.Redis):
+            self._redis.incr(method.__qualname__)
+        return method(self, *args, **kwargs)
+    return invoker
 
 
 class Cache:
@@ -38,9 +50,37 @@ class Cache:
         self._redis.set(data_key, data)
         return data_key
 
+    def get(self, key: str, fn: Callable[[bytes], Any] = None) -> Any:
+        """
+        Retrieves value from Redis data storage,
+        optionally converts it using fn.
+        """
+        value = self._redis.get(key)
+        if value is None:
+            return None
+        if fn is None:
+            return value
+        return fn(value)
+
+    def get_str(self, key: str) -> Any:
+        """Retrieves a string value from Redis data storage."""
+        return self.get(key, lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> Any:
+        """Retrieves an integer value from Redis data storage."""
+        return self.get(key, lambda d: int(d))
+
 
 if __name__ == "__main__":
     cache = Cache()
-    key = cache.store("Hello, Redis!")
-    print(f"Stored data with key: {key}")
+
+    TEST_CASES = {
+        b"foo": None,
+        123: int,
+        "bar": lambda d: d.decode("utf-8")
+    }
+
+    for value, fn in TEST_CASES.items():
+        key = cache.store(value)
+        assert cache.get(key, fn=fn) == value
 
