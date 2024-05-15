ba9164f7df3e7f4e3d95853c558df3e8cdb52fdc
bshongwe <dru.da.beat.hustle@gmail.com>
2024-05-15 04:31:40 -0700

    Tas 5: 🧩🔧 Fixing output to implement an expiring web cache and tracker - reco 2

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -9,14 +9,20 @@ from typing import Callable
 
 
 redis_store = redis.Redis()
-"""The module-level Redis instance."""
+"""
+The module-level Redis instance.
+"""
 
 
 def data_cacher(method: Callable) -> Callable:
-    """Caches the output of fetched data."""
+    """
+    Caches the output of fetched data.
+    """
     @wraps(method)
     def invoker(url) -> str:
-        """The wrapper function for caching the output."""
+        """
+        The wrapper function for caching the output.
+        """
         redis_store.incr(f'count:{url}')
         result = redis_store.get(f'result:{url}')
         if result:
