b10a7d6907e89955e8dc9dceb30fca45018e7470
bshongwe <dru.da.beat.hustle@gmail.com>
2024-05-15 10:40:17 -0700

    Task 5 - Advanced - 📣 Test libs

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -2,59 +2,41 @@
 """
 Module: Request caching and tracking tools
 """
-import requests
 import redis
+import requests
 from functools import wraps
-from typing import Callable
-
-"""
-Redis instance
-"""
-redis_instance = redis.Redis()
+from typing import Callable, Any, List, Dict
 
 
-def track_and_cache(method: Callable) -> Callable:
-    """Decorator to track and cache the results of a function."""
+redis_store = redis.Redis()
+'''The module-level Redis instance.
+'''
 
+
+def data_cacher(method: Callable) -> Callable:
+    """
+    Caches the output of fetched data.
+    """
     @wraps(method)
-    def wrapper(url: str) -> str:
+    def invoker(url) -> str:
         """
-        Wrapper function to track and cache results.
-        access_count_key: tracking access count
-        cache_key: cache result key
+        The wrapper function for caching the output.
         """
-        access_count_key = f"count:{url}"
-        cache_key = f"cache:{url}"
-
-        redis_instance.incr(access_count_key)
-
-        cached_result = redis_instance.get(cache_key)
-        if cached_result:
-            return cached_result.decode('utf-8')
-
+        redis_store.incr(f'count:{url}')
+        result = redis_store.get(f'result:{url}')
+        if result:
+            return result.decode('utf-8')
         result = method(url)
-
-        redis_instance.setex(cache_key, 10, result)
-
+        redis_store.set(f'count:{url}', 0)
+        redis_store.setex(f'result:{url}', 10, result)
         return result
+    return invoker
 
-    return wrapper
 
-
-@track_and_cache
+@data_cacher
 def get_page(url: str) -> str:
-    """Function to obtain the HTML content of a URL."""
-    response = requests.get(url)
-    return response.text
-
-
-if __name__ == "__main__":
     """
-    Test the get_page function
+    Returns content of URL after caching request's
+    response, and tracking request.
     """
-    url = "http://slowwly.robertomurray.co.uk"
-    print(get_page(url))
-    print(get_page(url))
-
-# 1st call delay, creates cache
-# 2nd call, brings back cache
+    return requests.get(url).text
