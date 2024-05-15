43345dbfa9748252da8648478748c577333c6247
bshongwe <dru.da.beat.hustle@gmail.com>
2024-05-14 22:18:50 -0700

    Edge tests init 🗃️

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+Module: Redis NoSQL data storage.
+"""
+import uuid
+import redis
+from typing import Union
+
+
+class Cache:
+    """Represents object for storing data in Redis data storage."""
+
+
+    def __init__(self) -> None:
+        """Initializes a Cache instance."""
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """Stores value in Redis data storage, returns the key."""
+        data_key = str(uuid.uuid4())
+        self._redis.set(data_key, data)
+        return data_key
