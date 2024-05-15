5bf9034c2948192feac90bd12bb2b7026dc9783e
bshongwe <dru.da.beat.hustle@gmail.com>
2024-05-15 00:54:34 -0700

    Task 34 🔧 Somthing is wrong, will fix it. 🔍 Add replay function to display call history of Cache methods 📝

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -9,7 +9,10 @@ from functools import wraps
 
 
 def call_history(func):
-    """Tracks call details of Cache class method."""
+    """
+    Tracks call details of Cache class method.
+    """
+
     @wraps(method)
     def invoker(self, *args, **kwargs) -> Any:
         """Returns method output after storing its inputs and output."""
@@ -25,7 +28,10 @@ def call_history(func):
 
 
 def count_calls(func):
-    """Tracks number of calls made to Cache class method."""
+    """
+    Tracks number of calls made to Cache class method.
+    """
+
     @wraps(method)
     def invoker(self, *args, **kwargs) -> Any:
         """Invokes given method after incrementing its call counter."""
@@ -35,13 +41,44 @@ def count_calls(func):
     return invoker
 
 
+def replay(fn: Callable) -> None:
+    """
+    Displays the call history of a Cache class' method.
+    """
+    if fn is None or not hasattr(fn, '__self__'):
+        return
+    redis_store = getattr(fn.__self__, '_redis', None)
+    if not isinstance(redis_store, redis.Redis):
+        return
+    fxn_name = fn.__qualname__
+    in_key = '{}:inputs'.format(fxn_name)
+    out_key = '{}:outputs'.format(fxn_name)
+    fxn_call_count = 0
+    if redis_store.exists(fxn_name) != 0:
+        fxn_call_count = int(redis_store.get(fxn_name))
+    print('{} was called {} times:'.format(fxn_name, fxn_call_count))
+    fxn_inputs = redis_store.lrange(in_key, 0, -1)
+    fxn_outputs = redis_store.lrange(out_key, 0, -1)
+    for fxn_input, fxn_output in zip(fxn_inputs, fxn_outputs):
+        print('{}(*{}) -> {}'.format(
+            fxn_name,
+            fxn_input.decode("utf-8"),
+            fxn_output,
+        ))
+
+
 class Cache:
-    """Represents object for storing data in Redis data storage."""
+    """
+    Represents object for storing data in Redis data storage.
+    """
 
     def __init__(self) -> None:
-        """Initializes a Cache instance."""
+        """
+        Initializes a Cache instance.
+        """
         self._redis = redis.Redis()
         self._redis.flushdb(True)
+
 
     @call_history
     @count_calls
