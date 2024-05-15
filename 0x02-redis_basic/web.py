d7139fe1a0afbde639865191d6de7e5f84f8c71d
bshongwe <dru.da.beat.hustle@gmail.com>
2024-05-15 04:49:54 -0700

    Tas 5: 🧩🔧 Fixing output: test

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -9,9 +9,8 @@ from typing import Callable
 
 
 redis_store = redis.Redis()
-"""
-The module-level Redis instance.
-"""
+'''The module-level Redis instance.
+'''
 
 
 def data_cacher(method: Callable) -> Callable:
